dbc9e58eb02b040452695dc87d88667520dc8085
Osim A <g-unit_osim@mail.ru>
2024-05-21 14:08:26 +0500

    filtering files and folders added

--- DirectoryList.py
+++ DirectoryList.py
@@ -2,26 +2,24 @@ import os
 import sys
 import argparse
 
-parse = argparse.ArgumentParser()
-
-filterFileType = '.c'
-filterFileType2 = '.h'
+filterFolderTypes = '',#'FreeRTOS_Sources', 'TestFolders'
+filterFileTypes = '.c', '.h', '.cpp', '.txt'
 
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            if f.endswith(filterFileType) or f.endswith(filterFileType2):
-                print('{}{}'.format(subindent, f))
+        for folderfltr in filterFolderTypes:
+            if root.endswith(folderfltr):
+                level = root.replace(startpath, '').count(os.sep)
+                indent = ' ' * 4 * (level)
+                print('{}{}/'.format(indent, os.path.basename(root)))
+                subindent = ' ' * 4 * (level + 1)
+                for f in files:
+                    for fltr in filterFileTypes:
+                        if f.endswith(fltr):
+                            print('{}{}'.format(subindent, f))
 
 def main():
-    #parse.add_argument('-p', '--path', type=str, default="deflt",
-                       #help=('path from where scanning directories begins' '(default: %(deflt)s'))
     print('__file__: ', __file__)
-    #if os.path.exist(args.path):
     res = ''.join(__file__).replace("/", "\\\\")
     print("current directory: ", res)
     res = ''.join(res).replace(os.path.basename(res), "")
